e0ad3a5dd7d91fa78c48a882f26674b96da45024
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-15 20:06:51 +0530

    Create channelData.js

---
+++ channelData.js
@@ -0,0 +1,20 @@
+const channelData = {
+  "Cricbuzz": {
+    "file": "https://live2.shoq.com.pk/live/eds/Criclife2/DASH/Criclife2.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "4301796d6d67374043c4a43c18dff7ea",
+        "key": "96a3dc8766317aa169149a604928ebb6"
+      }
+    }
+  },
+  "Willow": {
+    "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
+    "drm": {
+      "clearkey": {
+        "keyId": "59ed8dc9d4a48857ef8e5865919496cc",
+        "key": "0d3680bd8b349deda8b7fdd1da71b7f8"
+      }
+    }
+  }
+};
