ade951cac76ee956b5689430bf80125815dd0736
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-16 13:39:37 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -16,5 +16,23 @@ const channelData = {
         "key": "0d3680bd8b349deda8b7fdd1da71b7f8"
       }
     }
+  },
+  "Ptvsport": {
+    "file": "https://live4.shoq.com.pk/live/eds/PTV_Sports/DASH/PTV_Sports.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "9ecad6c4413f8bdc54712ce6c072a2cf",
+        "key": "442df559c369bdada8ba3abe97811575"
+      }
+    }
+  },
+  "Tntsport1": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "ae26845bd33038a9c0774a0981007294",
+        "key": "63ac662dde310cfb4cc6f9b43b34196d"
+      }
+    }
   }
 };
