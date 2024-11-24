623ce963c9af2ed31f6c6691bdad50867ad3ac5a
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-24 09:36:03 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -36,11 +36,11 @@ const channelData = {
     }
   },
   "s1hd": {
-    "file": "https://bpcdncs3.dialog.lk/bpk-token/2ab@ukjdzzlcfneh5mukcptpmsgsthvk0fthdbfjqxba/GlobalManifest.mpd",
+    "file": "https://bpcdncs1.dialog.lk/bpk-token/2aa@zbjthdlden4m2rqwamhv0sgpgooi0rebou5vhqca/GlobalManifest.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "506e9d88fcfc59cea6be2100b69da117",
-        "key": "11ed35df34df98373fbf8a45fb02cd44"
+        "keyId": "0b2e7d364dde5e69b645b74a596c3e57",
+        "key": "8d724d697c3649c7240b6cba298df0ff"
       }
     }
   },
