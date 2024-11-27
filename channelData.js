4c830334d44c7f65d4ca91d6a1c051b4f0bbe52a
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-27 13:48:12 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -35,12 +35,12 @@ const channelData = {
       }
     }
   },
-  "s1hd": {
-    "file": "https://bpcdncs1.dialog.lk/bpk-token/2aa@zbjthdlden4m2rqwamhv0sgpgooi0rebou5vhqca/GlobalManifest.mpd",
+  "trhd": {
+    "file": "https://dfwlive-v1-c1p3-sponsored.cf.dtvcdn.com/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
-        "keyId": "0b2e7d364dde5e69b645b74a596c3e57",
-        "key": "8d724d697c3649c7240b6cba298df0ff"
+        "keyId": "1f52934d28a25c51a5dd577a650dcece",
+        "key": "323a46b02a31172c2475406e56fd3be6"
       }
     }
   },
