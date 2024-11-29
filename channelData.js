1dc3ca1177e3c36da0932a9165aac9a9b3e16e1a
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-11-29 23:50:31 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -35,7 +35,7 @@ const channelData = {
       }
     }
   },
-  "Trhd": { // Capitalized the channel name for consistency
+  "WillowXtra": { // Capitalized the channel name for consistency
     "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.cf.dtvcdn.com/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
@@ -59,6 +59,24 @@ const channelData = {
       "clearkey": {
         "keyId": "4e993aa8c1f295f8b94e8e9e6f6d0bfe",
         "key": "86a1ed6e96caab8eb1009fe530d2cf4f"
+      }
+    }
+  },
+  "Tntsport4": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/f0qvkrra8j/out/v1/f8fa17f087564f51aa4d5c700be43ec4/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "6d1708b185c6c4d7b37600520c7cc93c",
+        "key": "1aace05f58d8edef9697fd52cb09f441"
+      }
+    }
+  },
+  "Tntsport4": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/i2pcjr4pe5/out/v1/912e9db56d75403b8a9ac0a719110f36/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "e31a5a81caff5d07ea2411a571fc2e59",
+        "key": "96c5ef69479732ae734f962748c19729"
       }
     }
   }
